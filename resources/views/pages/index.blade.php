cbe5f93d8b9efd8b45c813fe4d79909c03a0235b
AboodMadridista <41301312+AboodMadridista@users.noreply.github.com>
2019-04-02 23:26:57 +0800

    more shit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name','MallDirectory')}}</title>
+    </head> 
+    <body>
+       <h1>Welcome to the Mall Directory</h1>
+       <p>This is the Mall Directory for the assignment.. blah blah</p> 
+    </body>
+</html>
